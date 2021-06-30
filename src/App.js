f6fdbbe9a1c4baadb03980092799569cc597d96c
rogerwang2014 <rogerwang2014@gmail.com>
2021-06-30 23:29:02 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Button type="primary">页面A</Button>
+        <br />
+        <Button type="primary">页面B</Button>
+        <br />
+        <Button type="primary">页面C</Button>
+        <br />
+        <Button type="primary">页面D</Button>
+        <br />
+        <Button type="primary">页面E</Button>
+      </>
+    </div>
+  );
+}
+
+export default App;
